712549a3a41a1c01ccd3fba43c6b608f0ebdff21
Amin7775 <amin.dpi24@gmail.com>
2023-11-22 18:38:50 +0600

    Save User only if he doesn’t exists in the database

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ async function run() {
     //user related api
     app.post('/users', async(req,res)=>{
       const user = req.body;
+      const query = {email : user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'User Already Exist', insertedId: null})
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
